f5d345a922a24cdcdfe7c0fd4eee8ff4fd170725
Muhammad Raihan Al Ameer <77021024+RaihanAlAmeer@users.noreply.github.com>
2025-01-12 15:45:36 +0700

    js + css update

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -15,6 +15,7 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  let texts = document.getElementsByClassName("text2");
   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
@@ -24,6 +25,11 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+  for (i = 0; i < texts.length; i++) {
+    texts[i].style.display = "none";
+  }
+
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  texts[slideIndex-1].style.display = "block";
 }
